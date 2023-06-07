8757a86d8512e196cdba865959500618e6e213ec
safatsaat2 <sakibsafat47@gmail.com>
2023-06-08 02:16:10 +0600

    database connected

---
+++ index.js
@@ -0,0 +1,72 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors())
+app.use(express.json());
+
+
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.bqzilsj.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const usersCollection = client.db('photofyDB2').collection('usersCollection')
+
+
+    app.post('/users', async (req, res) => {
+        const user = req.body;
+        const query = { email: user.email }
+        const existedUser = await usersCollection.findOne(query);
+        if (existedUser) {
+            return res.send({ message: 'user already exists' })
+        }
+        const result = await usersCollection.insertOne(user);
+        res.send(result)
+    })
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('Photofy is running')
+})
+
+app.listen(port, () => {
+    console.log(`Photofy is running on ${port}  `)
+})
+
+
+        
