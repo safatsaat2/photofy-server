f45cb7f3e92f29604025d4f860bae5d0378012c8
safatsaat2 <sakibsafat47@gmail.com>
2023-06-11 15:10:00 +0600

    delete operation done

--- index.js
+++ index.js
@@ -15,31 +15,37 @@ app.use(express.json());
 
 app.post('/jwt', (req, res) => {
   const user = req.body;
-  const token = jwt.sign(user, process.env.SECRET_TOKEN, { expiresIn: '1h' })
+  const token = jwt.sign(user, process.env.SECRET_TOKEN, { expiresIn: '24h' })
 
   res.send({ token })
 })
 
 // JWT varyfying
-  const verifyJWT = (req, res, next)=>{
-    const authorization = req.headers.authorization;
-    if(!authorization){
-      return res.status(401).send({error: true, message: 'unauthorized access'})
-    }
-    const token = authorization.split(' ')[1];
-
-    jwt.verify(token, process.env.SECRET_TOKEN, (err, decoded) => {
-      if(err){
-        return res.status(401).send({error: true, message: 'unauthorized access'})
-      }
-      req.decoded = decoded;
-      next();
-    })
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'unauthorized access' });
   }
+  // bearer token
+  const token = authorization.split(' ')[1];
+  console.log("token",  token)
+  jwt.verify(token, process.env.SECRET_TOKEN, (err, decoded) => {
+    console.log("err", err)
+    if (err) {
+      return res.status(403).send({ error: true, message: 'unauthorized access' })
+    }
+    
+    req.decoded = decoded;
+    console.log("req.decoded", decoded);
+    next();
+  })
+}
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.bqzilsj.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -60,6 +66,44 @@ async function run() {
     const selectedClasses = client.db('photofyDB2').collection('selectedClasses')
 
 
+    // Varifying Admin 
+
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email }
+      const user = await usersCollection.findOne(query);
+      if (user?.role !== 'admin') {
+        return res.status(403).send({ error: true, message: 'forbidden message' });
+      }
+      next();
+    }
+
+// Admin
+
+
+app.get('/user/admin/:email', verifyJWT, async(req, res)=>{
+
+  const email = req.params.email;
+
+  if(email !== req.decoded.email){
+    res.send({admin: false})
+  }
+  const query ={email: email};
+  const user = await usersCollection.findOne(query);
+  const result = { admin: user?.role === 'admin' }
+  res.send(result)
+
+})
+
+
+
+//  Users collection
+
+    app.get('/users', verifyJWT, verifyAdmin, async(req, res)=>{
+      const result = await usersCollection.find().toArray();
+      res.send(result)
+    })
+
     app.post('/users', async (req, res) => {
       const user = req.body;
       const query = { email: user.email }
@@ -73,16 +117,31 @@ async function run() {
 
     // Instructor Collection
 
-    app.get('/instructor', async(req, res)=>{
-      const query = {role : 'instructor'}
-      const result = await  usersCollection.find(query).toArray()
+    app.get('/instructor', async (req, res) => {
+      const query = { role: 'instructor' }
+      const result = await usersCollection.find(query).toArray()
       res.send(result)
     })
 
 
     // selectedClasses collection
 
-    app.post('/selected-classes', verifyJWT,  async (req, res) =>{
+    app.delete('/selected-classes/:id', verifyJWT, async(req, res)=>{
+      console.log(req.decoded.email)
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await selectedClasses.deleteOne(query);
+      res.send(result)
+    })
+
+    app.get('/selected-classes/:email', async(req, res)=>{
+      const {email} = req.params;
+      const query = {email}
+      const result = await selectedClasses.find(query).toArray()
+      res.send(result)
+    })
+
+    app.post('/selected-classes', verifyJWT, async (req, res) => {
       const info = req.body;
       const result = await selectedClasses.insertOne(info)
       res.send(result)
@@ -90,7 +149,7 @@ async function run() {
 
     // class collection
 
-    app.get('/classes', async (req, res) => {
+    app.get('/classes',  async (req, res) => {
       const query = {};
       const options = {
         // sort matched documents in descending order by rating
